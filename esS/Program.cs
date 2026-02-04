55ec46153217f41fa67d9464cb5b0e1cee4d15bd
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-04 13:10:14 +0100

    Aggiungere i file di progetto.

---
+++ esS/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace esS
+{
+    internal class Program
+    {
+        static void Acronimo(string frase, string[] acronimo)
+        {
+            
+
+            for (int i = 0; i < acronimo.Length; i++)
+            {
+                Console.WriteLine(acronimo[i]);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Dammi una frase");
+            string frase = Console.ReadLine();
+            string[] acronimo = frase.Split(" ");
+            Acronimo(frase, acronimo);
+        }
+    }
+}

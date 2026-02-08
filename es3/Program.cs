1441bc746ac59e486f94e97d32c1d1a4fce6fbc0
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-08 11:50:45 +0100

    mod

---
+++ es3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace es3
+{
+    internal class Program
+    {
+        static void Conversione(string frase)
+        {
+            frase = frase.ToLower();
+            char[] f = frase.ToCharArray();
+            for (int i = 0; i < f.Length; i++)
+            {
+                if (i == 0 || f[i - 1] == ' ')
+                {
+
+                    f[i] = char.ToUpper(f[i]);
+
+
+                }
+                Console.Write(f[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi una frase");
+            string frase = Console.ReadLine();
+            Conversione(frase);
+        }
+    }
+}

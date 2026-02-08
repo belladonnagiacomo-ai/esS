24cdaa7d41e5d8ed8e2861d7a782ec785c290bac
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-08 13:42:06 +0100

    mod

---
+++ es9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace es9
+{
+    internal class Program
+    {
+        static void Inversione (string frase)
+        {
+            string[] parole = frase.Split(" ");
+            for (int i = parole.Length - 1; i >= 0; i--)
+            {
+
+                Console.Write(parole[i]);
+                Console.Write("  ");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi una frase");
+            string frase = Console.ReadLine();
+            Inversione (frase);
+        }
+    }
+}

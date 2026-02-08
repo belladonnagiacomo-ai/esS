a23e37818d4f2ddbbe66eba6ae7c1613be3aea27
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-02-08 12:15:32 +0100

    mod

---
+++ es6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace es6
+{
+    internal class Program
+    {
+        static void Slug (string frase)
+        {
+            frase = frase.ToLower ();
+            frase = frase.Trim ();
+            char[] f = frase.ToCharArray ();
+            for (int i = 0; i < f.Length; i++)
+            {
+                if(f [i] == ' ')
+                {
+                    f[i] = '-';
+                }
+                Console.Write(f[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dammi una frase");
+            string frase = Console.ReadLine();
+            Slug (frase);
+        }
+    }
+}
